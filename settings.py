1319f15430f0339c000fe0068d83286794c898a6
guillemfrancisco <gfrancisco@obsa.ad>
2021-06-16 17:20:07 +0200

    Fix error

--- settings.py
+++ settings.py
@@ -9,4 +9,4 @@ POSTGRE_DATABASE = os.environ['POSTGRE_DATABASE']
 ### API ###
 API_KEY = os.environ['API_KEY']
 SECRET_KEY = os.environ['SECRET_KEY']
-PUSH_URL_LOCATION = 'https://mooovement.actuatech.org/api/v0.1/notifications/push/'
+API_PUSH_URL = 'https://mooovement.actuatech.org/api/v0.1/notifications/push/'
